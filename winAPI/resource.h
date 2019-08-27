2b4aaaa9ebe08bc030830c8f44e223d7afed5acc
Jinho Song <sth4881@naver.com>
2019-08-28 02:11:16 +0900

    Updaing files

---
+++ winAPI/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// WinProject.rc에서 사용되고 있습니다.
+//
+#define IDB_BIMO                        101
+#define BIMO                            101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
