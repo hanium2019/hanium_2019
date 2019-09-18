446c1c9799a1aeecae5ddc4cacd982ddd8a1c2e8
Kim-SuBin <46712693+Kim-SuBin@users.noreply.github.com>
2019-09-18 23:01:41 +0900

    ScreenshotHandler
    Main camera view capture

---
+++ leapMotion/ScreenshotHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotHandler : MonoBehaviour
+{
+    private static ScreenshotHandler instance;
+    private Camera myCamera;
+    private bool takeScreenshotOnNextFrame;
+    public int FileCounter = 0;
+    private void Awake(){
+        instance = this;
+        myCamera = gameObject.GetComponent<Camera>();
+    }
+    private void OnPostRender(){
+        if (takeScreenshotOnNextFrame){
+            takeScreenshotOnNextFrame = false;
+            RenderTexture renderTexture = myCamera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/CameraScreenshot"+FileCounter + ".png", byteArray);
+            FileCounter++;
+            Debug.Log("Saved CameraScreenshot.png");
+
+            RenderTexture.ReleaseTemporary(renderTexture);
+            myCamera.targetTexture = null;
+        }
+    }
+    private void TakeScreenshot(int width, int height){
+        myCamera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        takeScreenshotOnNextFrame = true;
+    }
+
+    public static void TakeScreenshot_Static(int width, int height){
+        instance.TakeScreenshot(width, height);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            TakeScreenshot_Static(500,500);
+        }
+    }
+}
