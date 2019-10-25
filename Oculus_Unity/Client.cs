eae5b059a954fa35ab4f3df8520d7f01ddcf4cb6
Jinho Song <sth4881@naver.com>
2019-10-26 04:23:45 +0900

    Updaing Server & Client

--- Oculus_Unity/Client.cs
+++ Oculus_Unity/Client.cs
@@ -6,16 +6,10 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Client : MonoBehaviour
 {
-    /*private State state;
-
-    private enum State {
-        StartConnecting = 0,
-        CliecntCommunication,
-    }*/
-
     public class AsyncObject 
     {
         public byte[] buf;
@@ -23,88 +17,96 @@ public class Client : MonoBehaviour
         public AsyncObject(int bufSize) {
             this.buf = new byte[bufSize];
         }
+        public StringBuilder sb = new StringBuilder();
     }
 
-    private bool g_connected;
-
     private Socket clientSock = null;
-    private System.AsyncCallback fncConnectHandler;
-    private System.AsyncCallback fncReceiveHandler;
     private System.AsyncCallback fncSendHandler;
-
+    private System.AsyncCallback fncRecvHandler;
 
     // Start is called before the first frame update
     void Start()
     {
-        fncReceiveHandler = new System.AsyncCallback(ReceiveData);
-        fncSendHandler = new System.AsyncCallback(SendData);
-
-        clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
         IPAddress hostIP = IPAddress.Parse("127.0.0.1");
         IPEndPoint remoteEP = new IPEndPoint(hostIP, 11000);
 
-        bool isConnected = false;
+        fncSendHandler = new System.AsyncCallback(SendCallback);
+        fncRecvHandler = new System.AsyncCallback(RecvCallback);
+
+        clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
         try 
         {
             clientSock.Connect(remoteEP);
-            isConnected = true;
         }
 
         catch (SocketException e)
         {
             Debug.Log("Connection Error: " + e.Message);
-            isConnected = false;
         }
 
-        g_connected = isConnected;
-
-        if(isConnected) {
-            AsyncObject state = new AsyncObject(4096);
-            state.workSock = clientSock;
-            clientSock.BeginReceive(state.buf, 0, state.buf.Length,
-                SocketFlags.None, fncReceiveHandler, state);
-            Debug.Log("Connection Success!");
-        }
-        else Debug.Log("Connection Failed!");
+        SendMsg(clientSock, "This is a test<EOF>");
+        Receive(clientSock);
+        clientSock.Shutdown(SocketShutdown.Both);
+        clientSock.Close();
     }
 
-    void SendMsg(string msg) {
-        AsyncObject state = new AsyncObject(1);
-        state.buf = Encoding.Unicode.GetBytes(msg);
-        state.workSock = clientSock;
-        clientSock.BeginSend(state.buf, 0, state.buf.Length,
-            SocketFlags.None, fncSendHandler, state);
+    void SendMsg(Socket client, string msg) {
+        byte[] byteMsg = Encoding.Unicode.GetBytes(msg);
+        client.BeginSend(byteMsg, 0, byteMsg.Length,
+            SocketFlags.None, fncSendHandler, client);
     }
 
-    void ReceiveData(System.IAsyncResult ar) {
-        AsyncObject state = (AsyncObject) ar.AsyncState;
-        int recvBytes = state.workSock.EndReceive(ar);
-        if(recvBytes > 0) {
-            byte[] msgByte = new byte[recvBytes];
-            System.Array.Copy(state.buf, msgByte, recvBytes);
-            //Debug.Log("Received Msg: {0}", Encoding.Unicode.GetString(msgByte));
-        }
-        state.workSock.BeginReceive(state.buf, 0, state.buf.Length,
-            SocketFlags.None, fncReceiveHandler, state);
-    }
-
-    void SendData(System.IAsyncResult ar) {
-        AsyncObject state = (AsyncObject) ar.AsyncState;
-        int sentBytes = state.workSock.EndSend(ar);
-        if(sentBytes > 0) {
-            byte[] msgByte = new byte[sentBytes];
-            System.Array.Copy(state.buf, msgByte, sentBytes);
+    void SendCallback(System.IAsyncResult ar) {
+        try
+        {
+            Socket client = (Socket) ar.AsyncState;
+            int sentBytes = client.EndSend(ar);
             //Debug.Log("Send Msg: {0}", Encoding.Unicode.GetString(msgByte));
         }
+
+        catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+
     }
 
-    void StopConnecting() {
-        if(clientSock != null) {
-            clientSock.Close();
-            clientSock = null;
+    void Recv(Socket client) {
+        try
+        {
+            AsyncObject ao = new AsyncObject();
+            ao.workSock = client;
+            client.BeginReceive(ao.buf, 0, ao.buf.Length,
+            SocketFlags.None, fncRecvHandler, ao);
         }
-        Debug.Log("End client communication");
+        
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    void RecvCallback(System.IAsyncResult ar) {
+        string response = string.Empty;
+        try
+        {
+            AsyncObject ao = (AsyncObject) ar.AsyncState;
+            int recv = ao.workSock.EndReceive(ar);
+            if(recv > 0) {
+                ao.sb.Append(Encoding.Unicode.GetString(ao.buf, 0, recv));
+                ao.workSock.BeginReceive(ao.buf, 0, ao.buf.Length,
+                SocketFlags.None, fncRecvHandler, ao);
+            } else {
+                if(ao.sb.Length > 1) {
+                    response = ao.sb.ToString();
+                }
+
+            }
+        }
+
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
     }
 }
