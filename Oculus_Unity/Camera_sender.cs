e0de931f37f83ce9b1e1983921000b203f05f2f2
sejin kim <saejin7649@gmail.com>
2019-12-30 19:59:39 +0900

    Add share presenter's screen

---
+++ Oculus_Unity/Camera_sender.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+namespace TextureSendReceive {
+	[RequireComponent(typeof(Camera), typeof(TextureSender))]
+	public class Camera_sender : MonoBehaviour {
+		Camera camera;
+		TextureSender sender;
+		Texture2D sendTexture;
+        RenderTexture videoTexture;
+
+		public RawImage image;
+
+		// Use this for initialization
+		void Start () {
+			camera = GetComponent<Camera>();
+			sender = GetComponent<TextureSender>();
+			Debug.Log("camera.targetTexture.width");
+			Debug.Log(camera.targetTexture.width);
+
+			sendTexture = new Texture2D((int)camera.targetTexture.width, (int)camera.targetTexture.width);
+			
+			// Set send texture
+			sender.SetSourceTexture(sendTexture);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			RenderTexture.active = camera.targetTexture;
+			sendTexture.ReadPixels(new Rect(0, 0, camera.targetTexture.width, camera.targetTexture.height), 0, 0, false);
+				
+			// Set preview image target
+			image.texture = camera.targetTexture;
+		}
+	}
+}
